e5654fd146a52239e2158b6788774c8c605c3b12
Stuart Chapman <stuartpchapman@hotmail.com>
2016-05-29 15:33:11 +0800

    Create Create-SecurePullServer.ps1
    Configures the PowerShell DSC Web Pull Server using a self-signed certificate whose thumbprint is "0C53BAA5D940E19A88FD5D35BACEC4DC94D5AB00"

---
+++ Create-SecurePullServer.ps1
@@ -0,0 +1,54 @@
+#HTTPS
+configuration CreateSecurePullServer
+{
+  param
+  (
+	[string[]]$ComputerName = 'localhost',
+	
+	[ValidateNotNullOrEmpty()] 
+    [string] $certificateThumbPrint,
+  
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[String] $RegistrationKey
+  )
+  
+  Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+
+  Node $ComputerName
+  {
+    WindowsFeature DSCServiceFeature
+    {
+		Ensure	= "Present"
+		Name	= "DSC-Service"
+    }
+
+    xDscWebService PSDSCPullServer
+    {
+		Ensure 					= "Present"
+		EndpointName			= "PSDSCPullServer"
+		Port					= 8080
+		PhysicalPath			= "$env:SystemDrive\inetpub\PSDSCPullServer"
+		CertificateThumbPrint	= $certificateThumbPrint
+		ModulePath				= "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+		ConfigurationPath		= "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
+		State					= "Started"
+		DependsOn				= "[WindowsFeature]DSCServiceFeature"
+    }
+	
+	File RegistrationKeyFile
+	{
+		Ensure					= 'Present'
+		Type					= 'File'
+		DestinationPath			= "$env:PROGRAMFILES\WindowsPowerShell\DscService\RegistrationKeys.txt"
+		Contents				= $RegistrationKey
+	}
+
+  }
+
+}
+
+$certificates = Get-ChildItem -path cert:\LocalMachine\My
+$RegKey = [guid]::newGuid()
+CreateSecurePullServer -certificateThumbprint '0C53BAA5D940E19A88FD5D35BACEC4DC94D5AB00' -RegistrationKey $RegKey -OutputPath "C:\Configs\PullServer"
+Start-DscConfiguration -Path "C:\Configs\PullServer"
